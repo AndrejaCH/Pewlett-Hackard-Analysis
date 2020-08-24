8650298eb1b43dd72a186decb6d965f05f3f446c
Andreja Ho <sfandreja@gmail.com>
2020-08-24 15:09:47 -0700

    add retirement by dept table

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -26,7 +26,7 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Retirement eligibility
-SELECT first_name, last_name COUNT first_name
+SELECT COUNT(first_name), last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
@@ -92,9 +92,18 @@ ON ri.emp_no = de.emp_no;
 SELECT ri.emp_no,
     ri.first_name,
     ri.last_name,
-de.to_date
+    de.to_date
 INTO current_emp
 FROM retirement_info as ri
 LEFT JOIN dep_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_by_dept
+FROM current_emp as ce
+LEFT JOIN dep_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
