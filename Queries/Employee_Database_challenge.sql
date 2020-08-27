6dc7228c530ab0640a53af744fb183bbe1924186
Andreja Ho <sfandreja@gmail.com>
2020-08-26 22:41:15 -0700

    add deliverable3/ roles per title and dep

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,6 @@
+---------------------------------------------------------------------------------------------------------------------
 --Deliverable 1: 
+---------------------------------------------------------------------------------------------------------------------
 --The Number of Retiring Employees by Title.
 SELECT e.emp_no,
 	e.first_name, 
@@ -30,7 +32,9 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
 
+-----------------------------------------------------------------------------------------------------------------------------
 --Deliverable 2: 
+-----------------------------------------------------------------------------------------------------------------------------
 --The Employees Eligible for the Mentorship Program.
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no,
@@ -49,5 +53,38 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no, ti.from_date DESC;
+
+---------------------------------------------------------------------------------------------------------------------------
+--Deliverable 3:
+------------------------------------------------------------------------------------------------------------------------
+--Roles per Staff and Departament: 
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_titles_department
+FROM retirement_titles as rt
+INNER JOIN dep_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- How many roles will need to be fill per title and department?
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO rolls_to_fill
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+-- Qualified staff, retirement-ready to mentor next generation.
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO qualified_staff
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+WHERE ut.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
 
 
